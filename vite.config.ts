c1ed055dfecda1dc2416516011365e289e7b6e48
hiveMC3310 <HiveMC3310@yandex.ru>
2026-03-25 10:48:52 +0300

    rewrited vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import react from '@vitejs/plugin-react-swc'
-import { componentTagger } from 'lovable-tagger'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-	plugins: [react(), mode === 'development' && componentTagger()].filter(
+	plugins: [react()].filter(
 		Boolean
 	),
 	resolve: {
